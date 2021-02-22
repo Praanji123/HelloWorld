c73e5d2ebfd505cb08253b98c6b9e5ba5ff5158c
Prateek Srivastava <prateeksrivastava@Prateeks-MacBook-Pro.local>
2021-02-22 11:29:47 +0530

    added spring web dependency

--- src/main/java/com/example/SpringSession/SpringSessionApplication.java
+++ src/main/java/com/example/SpringSession/SpringSessionApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSessionApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello World");
 		SpringApplication.run(SpringSessionApplication.class, args);
+
 	}
 
 }
